f766b2ffa63e25a893651b13aebbda4df2d50c51
Rukaya Johaadien <r.johaadien@sanbi.org.za>
2015-04-30 11:10:21 +0200

    Fixing it so that the document list from rss appears in the correct place

--- wp-content/themes/bst/functions.php
+++ wp-content/themes/bst/functions.php
@@ -32,7 +32,7 @@ function display_all_authored_pages( $user ) {
     </ul>
 <?php
 }
-
+/*
 add_action( 'init', 'create_posttype' );
 function create_posttype() {
   register_post_type( 'seakey',
@@ -46,6 +46,47 @@ function create_posttype() {
       'rewrite' => array('slug' => 'seakeys'),
     )
   );
+}*/
+
+/* Rukaya's addition to show a list of documents recently added to www.sanbi.org tagged with biodiversity-advisor from the biodiversity advisor feed */
+
+function get_latest_ba_documents_func() {
+    
+//var_dump('http://www.sanbi.org/documents/biodiversity-advisor-feed http://www.sanbi.org/information/documents/feed');
+   // Get the RSS feed
+   $rss = simplexml_load_file('http://www.sanbi.org/information/documents/ba-feed');
+
+
+
+   // Set the number of words we want in the description for each item
+   $wordlength = 250; 
+   $output = '<div class="rss">';
+   foreach($rss->channel->item as $item) {
+        // Get the full description (this is all the text in the node)
+        $desc = $item->description;
+        
+        // Retrieve the first image
+        preg_match('/<img.+src=[\'"]\/sites\/default(?P<src>.+?)[\'"].*>/i', $desc, $image);
+        $image = $image['src'];
+        
+        // Select the actual field-field-description
+        preg_match('#<div class="field field-type-text field-field-description">.*\n.*\n.*\n.*\n(?P<desc>.+?)</div>#', $desc, $descfield);
+        $descfield = $descfield['desc'];
+                
+        // Add all the other elements into vars
+        $link = $item->link;
+        $title = $item->title;
+        $date = date('d F Y', strtotime($item->pubDate));
+        
+        // Print out the RSS item
+        $output = $output . '<article><h1><a href="' . $link  . '" title="' . $title . '">' . $title . '</a> <small>Published on ' . $date . '</small></h1>';
+        if($image) 
+            $output .= '<img src="http://www.sanbi.org/sites/default' . $image . '" alt="News item image" class="img-thumbnail">';
+        $output .= '<p>' . $descfield . '</p></article>';
+    }
+    $output .= '<p class="rss-footer">View more documents on <a href="http://www.sanbi.org/information/documents">sanbi.org</a></p></div>';
+    return $output;
 }
+add_shortcode( 'get_latest_ba_documents', 'get_latest_ba_documents_func' );
 
 ?>
